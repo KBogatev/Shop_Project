e3b083af8fa0ed4cc5d602cee1f405f7947bc37e
KBogatev <koko.bogatev@gmail.com>
2022-05-13 00:31:18 -0400

    init

--- API.py
+++ API.py
@@ -1,9 +1,15 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from sqlalchemy import create_engine, DateTime
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://Shop_Database:root@172.17.0.2:65529/shop_db'
+url = "mysql://root:root@localhost:52631"
+engine = create_engine(url)
+create_str = "CREATE DATABASE IF NOT EXISTS shop_db;"
+engine.execute(create_str)
+engine.execute("USE shop_db;")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:52631/shop_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 @app.route('/')
@@ -11,14 +17,17 @@ def index():
     return 'Hello!'
 
 
-class Users(db.model):
-    id = db.Column(db.integer, primary_key = True, auto_increment = True, nullable = False)
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True, nullable = False)
     first_name = db.Column(db.String(80), nullable = False)
     last_name = db.Column(db.String(80))
-    joined_at = db.Column(db.TIMESTAMP(datetime.now()))
+    joined_at = db.Column(DateTime(timezone=True), server_default=func.now(), nullable = False)
 
-class Products(db.model):
-    id = db.Column(db.integer, primary_key = True, nullable = False)
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
     name = db.Column(db.String(100), nullable = False)
-    created_at = db.Column(db.TIMESTAMP(datetime.now()))
+    created_at = db.Column(DateTime(timezone=True), server_default=func.now(), nullable = False)
     sell_state = db.Column(db.Boolean)
+
+db.create_all()
+db.session.commit()
