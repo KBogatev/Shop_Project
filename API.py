816c99255ee901ec2e05c17b39de4ee272f5cf7e
KBogatev <koko.bogatev@gmail.com>
2022-05-14 08:29:34 -0400

    init

--- API.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:50785/shop_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
